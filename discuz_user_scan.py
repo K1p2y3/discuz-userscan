f232f5272b2847c112efed92499e8380490814ad
KDC <kingchuang823@Gmail.com>
2021-08-02 19:12:04 +0800

    Create discuz_user_scan.py

---
+++ discuz_user_scan.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# -*- coding:UTF-8 -*-
+
+
+import requests
+import lxml.html
+import sys
+
+
+url = sys.argv[1]
+
+
+for i in range(1,10000):
+	html = requests.get(url= url+'/home.php?uid={0}'.format(i)).content
+	selector = lxml.html.fromstring(html)
+	res = selector.xpath("///a/text()")
+	res1 = selector.xpath("///a/@href")
+	print(res[-5])
