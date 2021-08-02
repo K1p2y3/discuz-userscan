85567d8a5a77c7a5e44274fe014e814b9e06a66f
KDC <kingchuang823@Gmail.com>
2021-08-02 19:13:12 +0800

    Update discuz_user_scan.py

--- discuz_user_scan.py
+++ discuz_user_scan.py
@@ -11,7 +11,7 @@ url = sys.argv[1]
 
 
 for i in range(1,10000):
-	html = requests.get(url= url+'/home.php?uid={0}'.format(i)).content
+	html = requests.get(url= url+'/home.php?uid={0}'.format(i),verify=False).content
 	selector = lxml.html.fromstring(html)
 	res = selector.xpath("///a/text()")
 	res1 = selector.xpath("///a/@href")
